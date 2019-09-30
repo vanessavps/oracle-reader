60bf7b38085062e75caa5ea6ed81d974a913a1b2
pereirav <vanessa.vps29@gmail.com>
2019-09-30 19:35:25 +1300

    Adding h2 to test database

---
+++ src/test/java/com/oraclereader/config/DatabaseTestConfig.java
@@ -0,0 +1,27 @@
+package com.oraclereader.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import javax.sql.DataSource;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
+
+@Configuration
+public class DatabaseTestConfig
+{
+  @Bean
+  @Primary
+  public DataSource dataSource() {
+    return new EmbeddedDatabaseBuilder()
+        .generateUniqueName(true)
+        .setType(H2)
+        .setScriptEncoding("UTF-8")
+        .ignoreFailedDrops(true)
+        //.addScript("schema.sql")
+        //.addScripts("user_data.sql", "country_data.sql")
+        .build();
+  }
+}
