cdc871dfa23119135564978ff150db8083ea7543
pereirav <vanessa.vps29@gmail.com>
2019-11-15 21:09:08 +1300

    Changing test properties name

--- src/test/java/com/oraclereader/controller/user/UserControllerIntegrationTest.java
+++ src/test/java/com/oraclereader/controller/user/UserControllerIntegrationTest.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Application.class)
 @AutoConfigureMockMvc
-@TestPropertySource(locations = "classpath:application-integrationtest.properties")
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class UserControllerIntegrationTest
 {
   @Autowired
